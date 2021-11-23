eddaa11ff20c65b803b2de7a3365c94ebb931623
Kouki Badr <badreddinekouki1998@gmail.com>
2021-11-23 20:36:33 +0100

    create base searchable listview widget with base parameters

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:searchable_listview/searchable_listview.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,103 +14,71 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(body: SafeArea(child: ExampleApp())),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class ExampleApp extends StatelessWidget {
+  List<User> users = [
+    User(age: 24, name: 'Badr'),
+    User(age: 27, name: 'Ali Mohamed'),
+    User(age: 29, name: 'Fathi'),
+    User(age: 50, name: 'Mohamed'),
+  ];
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  TextEditingController controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        children: [
+          Expanded(
+            child: SearchableList(
+              children: _generateWidgets(users),
+              onSearch: () {
+                var searchResult = users
+                    .where((element) => element.name.contains(controller.text))
+                    .toList();
+                return _generateWidgets(searchResult);
+              },
+              textEditingController: controller,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  List<Widget> _generateWidgets(List<User> listUsers) {
+    return List.generate(
+      listUsers.length,
+      (index) => Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Text(
+            listUsers[index].name,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class User {
+  int age;
+  String name;
+
+  User({required this.age, required this.name});
 }
--- lib/searchable_listview.dart
+++ lib/searchable_listview.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
 
-class SearchableList extends StatelessWidget {
-  const SearchableList({Key? key}) : super(key: key);
+class SearchableList extends StatefulWidget {
+  final List<Widget> children;
+  final Function onSearch;
+  final TextEditingController textEditingController;
+
+  const SearchableList({
+    Key? key,
+    required this.children,
+    required this.onSearch,
+    required this.textEditingController,
+  }) : super(key: key);
+
+  @override
+  State<SearchableList> createState() => _SearchableListState();
+}
+
+class _SearchableListState extends State<SearchableList> {
+
+  late List displayedList = widget.children;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        children: [
+          TextField(
+            controller: widget.textEditingController,
+            onChanged: (value){
+              setState(() {
+                displayedList = widget.onSearch();
+              });
+            },
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: displayedList.length,
+              itemBuilder: (context, index) => displayedList[index],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
