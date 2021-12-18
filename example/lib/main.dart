a9d605eda8a326296634671d5a5063f3cb1d3e66
Kouki Badr <badreddinekouki1998@gmail.com>
2021-12-18 10:08:57 +0100

    refactor example code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -59,44 +59,17 @@ class ExampleApp extends StatelessWidget {
               child: SearchableList<User>(
                 initialList: users,
                 keyboardAction: TextInputAction.search,
-                builder: (dynamic user) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Text(
-                        user.name,
-                        style: const TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  );
-                },
-                filter: (text) {
-                  return users
-                      .where(
-                        (element) => element.name.toLowerCase().contains(text),
-                      )
-                      .toList();
-                },
-                emptyWidget: Column(
-                  children: const [
-                    Icon(
-                      Icons.error,
-                      color: Colors.red,
-                    ),
-                    Text('No such user found'),
-                  ],
-                ),
+                builder: (dynamic user) => UserItem(user: user),
+                filter: _filterUserList,
+                emptyWidget: const EmptyView(),
                 inputDecoration: InputDecoration(
                   labelText: "Search",
                   fillColor: Colors.white,
                   focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(color: Colors.blue, width: 1.0),
+                    borderSide: const BorderSide(
+                      color: Colors.blue,
+                      width: 1.0,
+                    ),
                     borderRadius: BorderRadius.circular(10.0),
                   ),
                 ),
@@ -105,6 +78,59 @@ class ExampleApp extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+
+  _filterUserList(search) {
+    return users
+        .where(
+          (element) => element.name.toLowerCase().contains(search),
+        )
+        .toList();
+  }
+}
+
+class UserItem extends StatelessWidget {
+  final User user;
+
+  const UserItem({
+    Key? key,
+    required this.user,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Text(
+          user.name,
+          style: const TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class EmptyView extends StatelessWidget {
+  const EmptyView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        Icon(
+          Icons.error,
+          color: Colors.red,
+        ),
+        Text('No such user found'),
+      ],
     );
   }
 }
