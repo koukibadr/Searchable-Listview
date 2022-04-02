de89b440faf633eb29cc8b3a5596ec0f40815183
Kouki Badr <badreddinekouki1998@gmail.com>
2022-04-02 14:44:18 +0100

    add onrefresh attribute documentation

--- lib/searchable_listview.dart
+++ lib/searchable_listview.dart
@@ -109,6 +109,9 @@ class SearchableList<T> extends StatefulWidget {
   /// Defaults to [Colors.grey].
   final Color defaultSuffixIconColor;
 
+
+  ///An async callback invoked when dragging down the list
+  ///if onRefresh is nullable the drag to refresh is not applied
   final Future<void> Function()? onRefresh;
 
   @override
