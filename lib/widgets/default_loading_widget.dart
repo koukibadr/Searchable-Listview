b30b92493d75e897ed617b3e272595c340e268bb
koukibadr <badreddinekouki1998@gmail.com>
2026-08-26 17:36:37 +0100

    fix: upgrade flutter version and ui package to standalone packages

--- lib/widgets/default_loading_widget.dart
+++ lib/widgets/default_loading_widget.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:material_ui/material_ui.dart';
 
 class DefaultLoadingWidget extends StatelessWidget {
   const DefaultLoadingWidget({Key? key}) : super(key: key);
